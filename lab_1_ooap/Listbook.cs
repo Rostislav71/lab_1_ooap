0b9676a26f460b7222520bede4fd83db2f037199
Rostik Zavatskiy <ro.zavadskiy@gmail.com>
2021-11-29 11:52:54 +0200

    створив клас

---
+++ lab_1_ooap/Listbook.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_1_ooap
+{
+    public class Listbook
+    {
+        public int Number
+        {
+            get;
+            set;
+        }
+        public string Author
+        {
+            get;
+            set;
+        }
+        public int Year
+        {
+            get;
+            set;
+        }
+        public string Category
+        {
+            get;
+            set;
+        }
+
+        List<Listbook> homeLibraryList = new List<Listbook>();
+
+        public void Add(Listbook homeLibrary)
+        {
+            homeLibraryList.Add(homeLibrary);
+        }
+
+        public void Remove(Listbook homeLibrary)
+        {
+            homeLibraryList.Remove(homeLibrary);
+
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+
+            Listbook objHomeLibrary = obj as Listbook;
+            if (objHomeLibrary == null)
+                return false;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return Number + " " + Author + " " + Year + " " + Category;
+        }
+
+        public void ShowScreen()
+        {
+            foreach (var VARIABLE in homeLibraryList)
+            {
+                Console.WriteLine(VARIABLE.ToString());
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void Search(string str, int number)
+        {
+
+            foreach (var VARIABLE in homeLibraryList)
+            {
+                if (number == 1 && VARIABLE.Author == str)
+                {
+                    Console.WriteLine(VARIABLE.ToString());
+                }
+                else if (number == 2 && VARIABLE.Year == Convert.ToInt32(str))
+                {
+                    Console.WriteLine(VARIABLE.ToString());
+                }
+                else if (number == 3 && VARIABLE.Category == str)
+                {
+                    Console.WriteLine(VARIABLE.ToString());
+                }
+                else if (number == 4 && VARIABLE.Number == Convert.ToInt32(str))
+                {
+                    Console.WriteLine(VARIABLE.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ lab_1_ooap/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab_1_ooap
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Listbook homeLibrary = new Listbook();
+            homeLibrary.Add(new Listbook { Number = 1, Author = "Ессенин", Year = 1900, Category = "Стихи" });
+            homeLibrary.Add(new Listbook { Number = 2, Author = "Пушкин", Year = 1910, Category = "Рассказы" });
+            homeLibrary.Add(new Listbook { Number = 3, Author = "Лермонтов", Year = 1920, Category = "Басни" });
+            homeLibrary.Add(new Listbook { Number = 4, Author = "Достоевский", Year = 1910, Category = "Стихи" });
+            homeLibrary.ShowScreen();
+            homeLibrary.Remove(new Listbook { Number = 3, Author = "Лермонтов", Year = 1920, Category = "Басни" });
+            homeLibrary.ShowScreen();
+
+            Console.WriteLine("По какому критерию будем производить поиск? (По автору-1, год издания-2, по категории-3," +
+                              "по номеру -4)");
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Введите  писателя");
+                    homeLibrary.Search(Console.ReadLine(), number);
+                    break;
+                case 2:
+                    Console.WriteLine("Введите  год издания");
+                    homeLibrary.Search(Console.ReadLine(), number);
+                    break;
+                case 3:
+                    Console.WriteLine("Введите  категорию");
+                    homeLibrary.Search(Console.ReadLine(), number);
+                    break;
+                case 4:
+                    Console.WriteLine("Введите  номер книги");
+                    homeLibrary.Search(Console.ReadLine(), number);
+                    break;
+                default:
+                    Console.WriteLine("Не правильный критерий");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
